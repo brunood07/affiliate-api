0fbdc222725ba235731a89d2e66ee6ed7b02e596
brunood07 <brunood07@gmail.com>
2024-11-22 18:50:54 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {})
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://affiliate-ui.vercel.app',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
